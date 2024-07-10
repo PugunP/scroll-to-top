0e88db8b8f636cbc50a35744bc8a1e8bbc5a012f
Pugun <satapatjampa@gmail.com>
2024-07-10 17:46:13 +0700

    add to git

---
+++ app.js
@@ -0,0 +1,21 @@
+const scrollBtn = document.querySelector(".top")
+const rootEl = document.documentElement
+
+document.addEventListener("scroll",showBtn)
+scrollBtn.addEventListener("click",scrollTotop)
+
+function showBtn(){
+    const scrollTotal = rootEl.scrollHeight - rootEl.clientHeight
+    console.log((rootEl.scrollTop/scrollTotal)*100+"%")
+    if(rootEl.scrollTop/scrollTotal>0.3){
+        scrollBtn.classList.add("show-btn")
+    }else{
+        scrollBtn.classList.remove("show-btn")
+    }  
+}
+function scrollTotop(){
+    rootEl.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+}
